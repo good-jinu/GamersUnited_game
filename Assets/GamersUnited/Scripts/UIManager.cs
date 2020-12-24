8370a2ce71f442792dc5a4fa60310c6982867725
JINU <gooday6983@gmail.com>
2020-12-24 22:23:44 +0900

    Add PlayingUI which shows while playing game Update UIManger - Add ShowCurrentState function

--- Assets/GamersUnited/Scripts/UIManager.cs
+++ Assets/GamersUnited/Scripts/UIManager.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     private bool GameIsPaused = false;
+    private TextMeshProUGUI hp_barVal;
+    private TextMeshProUGUI ammo_barVal;
 
     public GameObject PauseMenu;
+    public GameObject hp_bar;
+    public GameObject ammo_bar;
 
     // Start is called before the first frame update
     void Start()
     {
         GameManager.Instance.UI = this;
+        hp_barVal = hp_bar.GetComponent<TextMeshProUGUI>();
+        ammo_barVal = ammo_bar.GetComponent<TextMeshProUGUI>();
     }
 
     private void Update()
     {
         if(Input.GetButtonDown("Cancel"))
         {
-            //esc키가 눌러졋을 때 일시정지
+            //Cancel("esc")버튼이 눌러졋을 때 일시정지
             if(GameIsPaused)
             {
                 Resume();
@@ -28,6 +35,7 @@ public class UIManager : MonoBehaviour
                 Pause();
             }
         }
+        ShowCurrentState();
     }
 
     //모든 UI 관련 작업을 수행할 클래스
@@ -48,6 +56,14 @@ public class UIManager : MonoBehaviour
         PauseMenu.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+
+    void ShowCurrentState()
+    {
+        //현재 체력과 총알 상태 텍스트로 업데이트
+        hp_barVal.text = "Hi";
+        ammo_barVal.text = "ss";
+        //위는 테스트 용
     }
     
     public void PrintDamage(float damage, Vector3 pos)
