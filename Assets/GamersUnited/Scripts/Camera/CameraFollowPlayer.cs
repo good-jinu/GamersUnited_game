b68b04bf96b12ce70f7b5f37eb0ef6ca5dc7e1d1
JINU <gooday6983@gmail.com>
2020-12-24 22:50:23 +0900

    Add CameraFollowPlayer.cs - make camera follow player

---
+++ Assets/GamersUnited/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    private Transform cameraPos;
+
+    public Vector3 offset;
+    public Transform playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerPos.position + offset;
+    }
+}
