6d7f88453f190289379932a506ba3cc1dfd8ca8c
JINU <gooday6983@gmail.com>
2020-12-24 16:12:57 +0900

    Update UIManeger.cs Add Resume, Pause function

--- Assets/GamersUnited/Scripts/UIManager.cs
+++ Assets/GamersUnited/Scripts/UIManager.cs
@@ -4,17 +4,51 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    private bool GameIsPaused = false;
+
+    public GameObject PauseMenu;
+
     // Start is called before the first frame update
     void Start()
     {
         GameManager.Instance.UI = this;
     }
 
+    private void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            //esc키가 눌러졋을 때 일시정지
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
     //모든 UI 관련 작업을 수행할 클래스
     //구현해야할 UI 목록
     //필수 : 플레이어 체력 표기, 탄환 갯수 표기, 일시정지 버튼 또는 키 입력시 일시정지 구현, 일시정지 UI 창에서 재시작/메뉴 창으로 선택지 구현
     //선택 : 공격 쿨타임 표기, 몬스터 체력 표기, 특정 키 입력시 현재 장비한 모든 아이템/무기 정보를 볼 수 있는 UI창 생성
+    void Resume()
+    {
+        //일시정지 화면에서 빠져나가기 Resume 할때 사용
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
 
+    void Pause()
+    {
+        //게임을 일시정지 시킬 때 사용
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
     
     public void PrintDamage(float damage, Vector3 pos)
     {
