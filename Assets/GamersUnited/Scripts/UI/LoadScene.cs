913458d24491d4cab357c2ae2693c4a9a97be9f8
JINU <gooday6983@gmail.com>
2021-01-02 11:54:21 +0900

    Add InitPlayerPrefs function Delete Test.cs

--- Assets/GamersUnited/Scripts/UI/LoadScene.cs
+++ Assets/GamersUnited/Scripts/UI/LoadScene.cs
@@ -13,5 +13,10 @@ namespace UIMan
         {
             SceneManager.LoadSceneAsync(sceneName);
         }
+
+        public void InitPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+        }
     }
 }
