324220ca5a95f2768787da40bb0c20b445ad0fdb
gusxo <chlgus1233@gmail.com>
2021-01-07 19:50:12 +0900

    Add new Scripts

---
+++ Assets/GamersUnited/Scripts/InstantObjectScripts/AttackInfo.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackInfo
+{
+    private readonly GameUnit _caster;
+    private readonly float _damage;
+    private readonly float _pushPower;
+    private readonly string _targetTag;
+    private readonly Vector3 _attackPosition;
+    private readonly int _enableHitCount;
+    private readonly AttackSuccessDelegate _attackSuccess;
+    private readonly AttackFailedDelegate _attackFailed;
+
+    public delegate void AttackSuccessDelegate(HitInfo info);
+    public delegate void AttackFailedDelegate(Vector3 position);
+
+    public GameUnit Caster { get => _caster;}
+    public float Damage { get => _damage;}
+    public float PushPower { get => _pushPower;}
+    public string TargetTag { get => _targetTag;}
+    public Vector3 AttackPosition { get => _attackPosition;}
+    public int EnableHitCount { get => _enableHitCount;}
+    public AttackSuccessDelegate AttackSuccess { get => _attackSuccess;}
+    public AttackFailedDelegate AttackFailed { get => _attackFailed;}
+
+    public AttackInfo(GameUnit caster,
+                      float damage,
+                      float pushPower,
+                      string targetTag,
+                      Vector3 attackPosition,
+                      int enableHitCount,
+                      AttackSuccessDelegate attackSuccess,
+                      AttackFailedDelegate attackFailed)
+    {
+        _caster = caster;
+        _damage = damage;
+        _pushPower = pushPower;
+        _targetTag = targetTag;
+        _attackPosition = attackPosition;
+        _enableHitCount = enableHitCount;
+        _attackSuccess = attackSuccess;
+        _attackFailed = attackFailed;
+    }
+    public AttackInfo(GameUnit caster,
+                      float damage,
+                      float pushPower,
+                      string targetTag,
+                      Vector3 attackPosition) : this(caster, damage, pushPower, targetTag, attackPosition, int.MaxValue, null, null) { }
+    public AttackInfo(GameUnit caster,
+                      float damage,
+                      float pushPower,
+                      string targetTag,
+                      Vector3 attackPosition,
+                      int enableHitCount) : this(caster, damage, pushPower, targetTag, attackPosition, enableHitCount, null, null) { }
+    public AttackInfo(GameUnit caster,
+                      float damage,
+                      float pushPower,
+                      string targetTag,
+                      Vector3 attackPosition,
+                      int enableHitCount,
+                      AttackSuccessDelegate attackSuccess) : this(caster, damage, pushPower, targetTag, attackPosition, enableHitCount, attackSuccess, null) { }
+    public AttackInfo(GameUnit caster,
+                      float damage,
+                      float pushPower,
+                      string targetTag,
+                      Vector3 attackPosition,
+                      int enableHitCount,
+                      AttackFailedDelegate attackFailed) : this(caster, damage, pushPower, targetTag, attackPosition, enableHitCount, null, attackFailed) { }
+}
+
---
+++ Assets/GamersUnited/Scripts/InstantObjectScripts/HitInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitInfo
+{
+    private readonly GameUnit _hitUnit;
+    private readonly float _damage;
+    private readonly Vector3 _hitPosition;
+    public HitInfo(GameUnit hitUnit, float damage, Vector3 hitPosition)
+    {
+        _hitUnit = hitUnit;
+        _damage = damage;
+        _hitPosition = hitPosition;
+    }
+
+    public Vector3 HitPosition => _hitPosition;
+
+    public float Damage => _damage;
+
+    public GameUnit HitUnit => _hitUnit;
+}
